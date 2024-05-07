89344f47e48db91a2fb64f4815e3691054a8ded1
lons <lonsnw@gmail.com>
2024-05-07 11:24:56 -0500

    Breaking MovieList out to another component, MovieCard

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,55 @@
+import { Typography, Card, Stack, CardContent, CardMedia, Paper } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { styled } from '@mui/material/styles';
+import { useHistory } from 'react-router-dom';
+
+function MovieCard() {
+    const dispatch = useDispatch();
+    const movies = useSelector(store => store.movies);
+    const history = useHistory();
+
+  // sending movie ID to saga to load when details page opens?
+    const getID = (movieId) => {
+        dispatch({ type: 'SEND_ID', payload: movieId})
+        history.push('/details')
+    }
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  }));
+
+    return(
+        <Item className="movies">
+          {movies.map(movie => {
+            return (
+              <Card 
+                data-testid='movieItem' 
+                key={movie.id} 
+                sx={{ bgcolor: "secondary", padding: 3, width: 240 }}>
+                  <Stack 
+                    direction="column" 
+                    justifyContent="space-evenly" 
+                    alignItems="center" 
+                    spacing={2}>
+                        <CardContent>
+                            <Typography variant="h6">{movie.title}</Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            alt={movie.title}
+                            height="300"
+                            image={movie.poster} 
+                            data-testid="toDetails"
+                            onClick={() => {getID(movie.id)}} />
+                  </Stack>
+              </Card>
+            );
+          })}
+        </Item>
+    )
+}
+
+export default MovieCard;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,34 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Typography, Grid, Card, Stack, CardContent, CardMedia, Box, Paper } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Grid, Box } from '@mui/material';
 import './MovieList.css';
+import MovieCard from '../MovieCard/MovieCard'
 import CollectionDetails from '../CollectionDetails/CollectionDetails';'../CollectionDetails/CollectionDetails'
-import { useHistory } from 'react-router-dom';
 
 function MovieList() {
 
   const dispatch = useDispatch();
-  const movies = useSelector(store => store.movies);
-  const history = useHistory();
 
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
-
-  // sending movie ID to saga to load when details page opens?
-  const getID = (movieId) => {
-    dispatch({ type: 'SEND_ID', payload: movieId})
-    history.push('/details')
-  }
-
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-  }));
 
   return (
     <div className='movie'>
@@ -40,33 +24,7 @@ function MovieList() {
             justifyContent="space-evenly"
             alignItems="center">
               <Grid item>
-                <Item className="movies">
-                  {movies.map(movie => {
-                    return (
-                      <Card 
-                        data-testid='movieItem' 
-                        key={movie.id} 
-                        sx={{ bgcolor: "secondary", padding: 3, width: 240 }}>
-                          <Stack 
-                            direction="column" 
-                            justifyContent="space-evenly" 
-                            alignItems="center" 
-                            spacing={2}>
-                              <CardContent>
-                                <Typography variant="h6">{movie.title}</Typography>
-                              </CardContent>
-                              <CardMedia
-                                component="img"
-                                alt={movie.title}
-                                height="300"
-                                image={movie.poster} 
-                                data-testid="toDetails"
-                                onClick={() => {getID(movie.id)}} />
-                          </Stack>
-                      </Card>
-                    );
-                  })}
-                </Item>
+                <MovieCard />
               </Grid>
           </Grid>
     </Box>
